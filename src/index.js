dc33a5264b36fde8752421edf6a3f7d68d734d1d
Simon Kateregga <spkateregga@gmail.com>
2020-12-04 22:22:46 -0600

    Minor clean up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Set up the React app from scratch
+// Render an App component (defined in a separate file)
+// Inside App, render:
+  // 1. A Navbar component
+  // 2. A MainContent component
+  // 3. A Footer component
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'))
