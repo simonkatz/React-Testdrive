3e5c4dd7ffe1fe896a422365a5e845877382d527
Simon Kateregga <spkateregga@gmail.com>
2020-12-07 19:21:58 -0600

    Clean up

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function Footer () {
-    return (
-        <footer className='footer'> This is a great footer</footer>
-    )
-}
-export default Footer
--- src/index.js
+++ src/index.js
@@ -2,10 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
 import './style.css'
-// From scratch, initialize the React app
-// Render an <App /> component
-// Create the <App /> component from scratch
-// Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
-// like you're making a todo list with some hard-coded items on it
 
 ReactDOM.render(<App />, document.getElementById('root'))
