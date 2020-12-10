8f2131c491004645812deb4315de6a4e3fdf58a6
Simon Kateregga <spkateregga@gmail.com>
2020-12-09 23:45:20 -0600

    Mapping components

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -5,7 +5,7 @@ function Joke(props) {
 		<div className="jokes">
 			<div className="joke">
             	<h3 style={{display: props.joke.question ? "block" : "none"}}>Question: {props.joke.question}</h3>
-				<p>Answer: {props.joke.punchline}</p>
+				<p>Answer: {props.joke.punchLine}</p>
 				<hr/>
 			</div>
 		</div>
