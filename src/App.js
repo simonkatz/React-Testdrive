afebea43221655558b906c00960674af5d5ff526
Simon Kateregga <spkateregga@gmail.com>
2020-12-11 16:58:06 -0600

    React ToDo app with mapped todo items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import Product from './Product'
-import productData from './productData'
+import TodoItem from './TodoItem'
+import todosData from './todosData'
 
 function App() {
-	const productComponents = productData.map(product => <Product key={product.id} name={product.name} price={product.price} description={product.description} />)
+	const todoComponents = todosData.map(item => <TodoItem key={item.id} item={item} />)
 	return (
 		<div>
-			{productComponents}
+			{todoComponents}
 		</div>
 	)
-} 
+}
+
 export default App
