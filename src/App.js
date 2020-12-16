bdaac2f0623ba70d7416d8cf490bb48011f2ec57
Simon Kateregga <spkateregga@gmail.com>
2020-12-16 16:00:08 -0600

    Todo app with setState and events. Phase 5 & 6

--- src/App.js
+++ src/App.js
@@ -8,10 +8,27 @@ class App extends Component {
 		this.state = {
 			todos: todosData
 		}
+		this.handleChange = this.handleChange.bind(this)
 	}
-
+	handleChange(id) {
+		this.setState(prevState => {
+			const updatedTodos = prevState.todos.map(todo => {
+				if(todo.id === id)
+				{
+					return {
+						...todo,
+						completed: !todo.completed
+					}
+				}
+				return todo
+			})
+			return {
+				todos: updatedTodos
+			}
+		})
+	}
 	render() {
-		const todoComponents = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+		const todoComponents = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
 		return (
 			<div>
 				{todoComponents}
