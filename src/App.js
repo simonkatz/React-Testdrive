a0687a2c5983b6e4101474a12d8bb6c86854cbe1
Simon Kateregga <spkateregga@gmail.com>
2020-12-16 18:33:20 -0600

    Fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-import React, {Component} from 'react'
-import TodoItem from './TodoItem'
-import todosData from './todosData'
+import React, {Component} from "react"
 
 class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			todos: todosData
-		}
-		this.handleChange = this.handleChange.bind(this)
-	}
-	handleChange(id) {
-		this.setState(prevState => {
-			const updatedTodos = prevState.todos.map(todo => {
-				if(todo.id === id)
-				{
-					return {
-						...todo,
-						completed: !todo.completed
-					}
-				}
-				return todo
-			})
-			return {
-				todos: updatedTodos
-			}
-		})
-	}
-	render() {
-		const todoComponents = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-		return (
-			<div>
-				{todoComponents}
-			</div>
-		)
-	}
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            character: {}
+        }
+    }
+    
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("https://swapi.dev/api/people/1/")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    character: data
+                })
+            })
+    }
+    
+    render() {
+        const text = this.state.loading ? "loading..." : this.state.character.name
+        return (
+            <div>
+                <p>{text}</p>
+            </div>
+        )
+    }
 }
 
 export default App
