f2efbce82b6b804136b05bf17bda7cd000626e3b
Simon Kateregga <spkateregga@gmail.com>
2020-12-16 20:29:19 -0600

    React forms and controlled components

--- src/App.js
+++ src/App.js
@@ -4,29 +4,124 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            loading: false,
-            character: {}
+            firstName: "",
+            lastName: "",
+            age: "",
+            gender: "",
+            isChicken: true,
+            isSeafood: false,
+            isPork: false
         }
+        this.handleChange = this.handleChange.bind(this)
     }
-    
-    componentDidMount() {
-        this.setState({loading: true})
-        fetch("https://swapi.dev/api/people/1/")
-            .then(response => response.json())
-            .then(data => {
-                this.setState({
-                    loading: false,
-                    character: data
-                })
-            })
+    handleChange(event) {
+        const {name, value, type, checked} = event.target;
+        
+        type === "checkbox" ? 
+            this.setState({
+                [name]: checked
+            }) 
+        :
+        this.setState({
+            [name]: value
+        })
     }
     
     render() {
-        const text = this.state.loading ? "loading..." : this.state.character.name
         return (
-            <div>
-                <p>{text}</p>
-            </div>
+            <main>
+                <form>
+                    <input 
+                        name="firstName" 
+                        value={this.state.firstName} 
+                        placeholder="First Name" 
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <input 
+                        name="lastName" 
+                        value={this.state.lastName} 
+                        placeholder="Last Name" 
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <input 
+                        name="age" 
+                        value={this.state.age} 
+                        placeholder="Age" 
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <label>Gender</label>
+                    <input 
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        checked={this.state.gender === "male" }
+                        onChange={this.handleChange}
+                    /> Male
+                    <input 
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        checked={this.state.gender === "female" }
+                        onChange={this.handleChange}
+                    /> Female
+                    <br />
+                    
+                    <select 
+                        name="destination"
+                        onChange={this.handleChange}
+                    >
+                        <option value="Uganda">Uganda</option>
+                        <option value="Rwanda">Rwanda</option>
+                        <option value="Kenya">Kenya</option>
+                    </select>
+                    <br />
+                    
+                    <input 
+                        type="checkbox"
+                        name="isChicken"
+                        onChange={this.handleChange}
+                        checked={this.state.isChicken}
+                    /> Chicken
+                    <br />
+                    
+                    <input 
+                        type="checkbox"
+                        name="isSeafood"
+                        checked={this.state.isSeafood}
+                        onChange={this.handleChange}
+                    /> Seafood
+                    
+                    <br />
+                    
+                    <input 
+                        type="checkbox"
+                        name="isPork"
+                        checked={this.state.isPork}
+                        onChange={this.handleChange}
+                    /> Pork
+                    
+                    <br />
+                    <button>Submit</button>
+                </form>
+                <hr />
+                <h2>Entered information:</h2>
+                <p>Your name: {this.state.firstName}, {this.state.lastName}</p>
+                <p>Your age: {this.state.age}</p>
+                <p>Your gender: {this.state.gender}</p>
+                <p>Your destination: {this.state.destination}</p>
+                <p>
+                    Your dietary restrictions: 
+                    <br/>
+                    Chicken: {this.state.isChicken ? "Yes" : "No"}
+                    <br/>
+                    Seafood: {this.state.isSeafood ? "Yes" : "No"}
+                    <br/>
+                    Pork: {this.state.isPork ? "Yes" : "No"}
+                </p>
+            </main>
         )
     }
 }
